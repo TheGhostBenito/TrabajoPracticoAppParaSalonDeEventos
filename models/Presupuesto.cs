b7fb183fb021f174bbb373da28b487ff41613d02
El Fantasma Benito <BenitoTGB@outlook.com.ar>
2026-09-09 15:08:48 -0300

    primer commit

---
+++ models/Presupuesto.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proyecto_integrador.models
+{
+    public class Presupuesto
+    {
+        private int id ;
+        private DateTime fechaCreacionDePresupuesto ;
+        private int duracionDelEventoEnHoras ;
+        private int cantidadDeAdultosQueAsisten;
+        private int cantidadDeNiñosQueAsisten;
+        private Salon salonDelEvento;
+        private MenuDeComida menuDeComidaSegunAsistencia;
+        private List<ServiciosAdicionales> serviciosAdicionales;
+        private TipoDeExclusionDeMenu exclusionDeMenu;
+
+
+        public Presupuesto(Salon _salon, MenuDeComida _menu, List<ServiciosAdicionales> _servicios, TipoDeExclusionDeMenu _exclusion)
+        {
+            salonDelEvento = _salon;
+            menuDeComidaSegunAsistencia = _menu;
+            serviciosAdicionales = _servicios;
+            exclusionDeMenu = _exclusion;
+            fechaCreacionDePresupuesto = DateTime.Now;
+        }
+
+
+        public decimal CalcularCostoTotal()
+        {
+            decimal total = Salon.costoBase;
+            total += (cantidadDeAdultosQueAsisten * menuDeComidaSegunAsistencia.costoPorAdulto);
+            total += (cantidadDeNiñosQueAsisten * menuDeComidaSegunAsistencia.costoPorNiño);
+            foreach (var servicio in serviciosAdicionales)
+            {
+                total += servicio.costoDelServicio;
+            }
+            return total;
+        }
+    }
+
+
+}
---
+++ models/Salon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto_integrador.models
+{
+    public class Salon
+    {
+        private int id;
+        private string nombreDelSalon;
+        private decimal costoBase;
+
+
+
+        public Salon(int _id, string _nombreDelSalon, decimal _costoBase)
+        {
+            id = _id;
+            nombreDelSalon = _nombreDelSalon;
+            costoBase = _costoBase;
+        }
+
+    }
+
+}
